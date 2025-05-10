e292caf8d964851a96629850dd00f317312b058c
Michael Ball <michael@mball.co>
2025-05-10 13:58:40 +0200

    Add a few more blocks and fixup some labels

--- scripts/make-blocks.rb
+++ scripts/make-blocks.rb
@@ -34,9 +34,22 @@ example_images:
 {{< include ../_block.qmd >}}
 TEMPLATE
 
+SYMBOL_MAP = {
+  "$greenflag": "<img src=\"/images/greenflag.png\" alt=\"Green Flag\" class=\"icon\" />",
+  "$clockwise": "↻",
+  "$counterclockwise": "↺",
+  "$arrowRight": "→",
+  "$pause": "⏸",
+}
+
 def title(label)
   # Remove ":" from the label which seems to cause issues with YAML
   label.gsub(':', '').gsub('_', '').capitalize
+  # Replace $symbols with their HTML equivalents
+  SYMBOL_MAP.each do |symbol, html|
+    label.gsub!(symbol.to_s, html)
+  end
+  label
 end
 
 def block_template(block)
@@ -50,15 +63,32 @@ def block_template(block)
 
   template
 end
+
+# This file is exported from this Snap! project:
+# https://snap.berkeley.edu/versions/dev/snap.html#present:Username=cycomachead&ProjectName=markdown%20blocks%20table
 blocks_json = JSON.parse(File.read('scripts/blocks.json'))
 
 puts "Generating blocks... #{blocks_json.length} blocks"
 
+all_custom_labels = []
+
 blocks_json.each do |block|
   block_page = block_template(block)
+  # All $words in the label
+  block['label'].scan(/\$[a-zA-Z0-9_]+/).each do |custom_label|
+    all_custom_labels << custom_label
+  end
   FileUtils.mkdir_p("#{dest}/#{block['category']}")
+  # TODO: Figure out how to merge existing files
+  # File.open("#{dest}/#{block['category']}/#{block['selector']}.qmd", 'a') do |f|
+  #   f.write(block_page)
+  # end
   File.open("#{dest}/#{block['category']}/#{block['selector']}.qmd", 'w') do |f|
     f.write(block_page)
   end
   puts "  - blocks/#{block['category']}/#{block['selector']}.qmd"
 end
+
+all_custom_labels.uniq!
+puts "Found #{all_custom_labels.length} custom labels"
+puts "Custom labels:\n#{all_custom_labels.join("\n")}"
